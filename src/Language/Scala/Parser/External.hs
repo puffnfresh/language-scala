31e5122eaeb5a69798fc5e0ab252cf7745b1ba9f
Brian McKenna <brian@brianmckenna.org>
2020-10-19 11:10:15 +0000

    Add Haddock for External and export NodeResult

--- src/Language/Scala/Parser/External.hs
+++ src/Language/Scala/Parser/External.hs
@@ -5,7 +5,7 @@ Calls out to the scalameta-parsers npm library using node. The library parses
 Scala code and dumps out the AST in JSON.
 -}
 
-module Language.Scala.Parser.External (runNodeScalametaParsers, nodeParser) where
+module Language.Scala.Parser.External (NodeResult (..), runNodeScalametaParsers, nodeParser) where
 
 import Control.Applicative (Alternative ((<|>)))
 import Data.Aeson
@@ -20,6 +20,7 @@ import Language.Scala (Source)
 import Paths_language_scala (getDataFileName)
 import System.Process (readProcess)
 
+-- | The 'FromJSON' instance decodes an error or a 'Source' value.
 data NodeResult
   = NodeError String Word Word
   | NodeSuccess Source
@@ -40,11 +41,15 @@ instance FromJSON NodeResult where
           )
           a
 
+-- | Runs the npm library "scalameta-parsers" on the input Scala code. Outputs
+-- JSON representing the Scala syntax tree.
 runNodeScalametaParsers :: String -> IO String
 runNodeScalametaParsers s = do
   script <- getDataFileName "scalameta-parsers-to-json.js"
   readProcess "node" [script] s
 
+-- | Decodes the result of the npm library "scalameta-parsers" using JSON. Can
+-- use 'NodeResult' to handle errors.
 nodeParser :: FromJSON a => String -> IO (Either String a)
 nodeParser =
   (eitherDecode . encodeUtf8 . pack <$>) . runNodeScalametaParsers
